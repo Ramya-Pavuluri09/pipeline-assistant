6a353f3b6e59b421c2deef1b2ad24eb51bef4dd1
Pavuluri <ramya.pavuluri@accenture.com>
2025-12-19 21:09:32 +0530

    initial commit

---
+++ src/main/java/com/pipeline/assistant/PipelineAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.pipeline.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipeline/assistant/PipelineAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
